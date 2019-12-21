6834c238e912f700b4787c21f7c240d1612db9d5
Ethan Miller <ethanzanemiller@gmail.com>
2019-12-21 11:22:51 -0500

    Update walkthrough

--- src/walkthrough.clj
+++ src/walkthrough.clj
@@ -1,6 +1,7 @@
 (ns walkthrough
   (:require [tech.ml.dataset :as ds]
             [tech.v2.datatype :as dtype]
+            [tech.v2.datatype.functional :as dfn]
             [tech.ml.dataset.column :as ds-col]))
 
 
@@ -16,12 +17,9 @@
 
 ;; Manipulation
 (def new-ds (ds/->dataset [{:a 1 :b 2} {:a 2 :c 3}]))
-
 (first new-ds)
 (last new-ds)
-
 (new-ds :c)
-
 (ds-col/missing (new-ds :b))
 (ds-col/missing (new-ds :c))
 
@@ -44,3 +42,31 @@
 
 ;; read a row
 (ds/value-reader nameage)
+
+;; ames - sub-rect selection
+(def ames-ds (ds/->dataset "file://data/train.csv"))
+
+(ames-ds "KitchenQual")
+(ames-ds "SalePrice")
+
+;; "subrect" selection
+(def small-ames (ds/select ames-ds ["KitchenQual" "SalePrice"] (range 5)))
+
+;; add, update, remove columns
+(ds/update-column small-ames "SalePrice" #(-> (dtype/->reader % :float64)
+                                              dfn/log))
+(ds/add-or-update-column small-ames "Range" (float-array (range 5)))
+(ds/remove-column small-ames "KitchenQual")
+
+;; filter
+(-> (ds/ds-filter #(< 30000 (get % "SalePrice")) ames-ds)
+    (ds/select ["SalePrice" "KitchenQual"] (range 5)))
+
+;; sort-by
+(-> (ds/ds-sort-by #(get % "SalePrice") ames-ds)
+    (ds/select ["SalePrice" "KitchenQual"] (range 5)))
+;; - what if you want to sort descending? 
+
+(def group-map (->> (ds/select ames-ds ["SalePrice" "KitchenQual"] (range 20))
+                    (ds/ds-group-by #(get % "KitchenQual"))))
+;; output here is a bit awkward
